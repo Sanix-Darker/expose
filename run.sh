44151908393d9ec131070cdf56f1b63cc8fe9132
sanix-darker <s4nixd@gmail.com>
2023-03-15 08:16:21 +0100

    fix: the expose link

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # To get the script
-wget https://gist.githubusercontent.com/Sanix-Darker/d70fc117a1a164277fe2a41bac66ff14/raw/64e39b9bc7b5d31c3a4fa3f0cd387f583d626038/expose -P $HOME/
+wget https://raw.githubusercontent.com/Sanix-Darker/expose/master/expose -P $HOME/
 # To add execution rights to the script
-chmod +x expose
+chmod +x $HOME/expose
 
 # To add an alias to run it on any directory
 # You can add it on your ~/.bashrc if you want
 echo "alias expose='$HOME/expose'" >> ~/.bashrc
 # echo "alias expose='$HOME/expose'" >> ~/.zshrc
+
+# We need to source the changes made
+source ~/.bashrc
+# source ~/.zshrc
 
 # To run it you just have to hit:
 # expose 9090
